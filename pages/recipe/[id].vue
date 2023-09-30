3541bb9d84463ae76f021ca8bcc0f1a926bbdcd6
Andreas Gerasimow <andreas.gerasimow12@gmail.com>
2023-09-30 19:54:13 +0200

    Recipe preview bug fix

--- pages/recipe/[id].vue
+++ pages/recipe/[id].vue
@@ -5,15 +5,16 @@
     <div v-else class="flex flex-col items-center">
         <div class="w-80 flex flex-col items-start gap-5">
             <h1 class="font-bold text-3xl">{{ recipe.name }}</h1>
-            <p class="btn font-bold px-3 py-1 rounded-xl bg-stone-300">Type: {{ recipe.type }}</p>
+            <p class="btn font-bold px-3 py-1 rounded-xl bg-stone-300">Type: {{ type }}</p>
             <div>
                 <p class="font-bold">Ingredients:</p>
-                <p v-for="ingredient in recipe.ingredients">{{ ingredient.name }}, {{ ingredient.amount }}{{ ingredient.unit
-                }}</p>
+                <p v-for="ingredient in recipe.ingredients">
+                    {{ ingredient.name }}, {{ ingredient.amount }}{{ ingredient.unit }}
+                </p>
             </div>
             <div>
                 <p class="font-bold">Instructions:</p>
-                <p>{{ recipe.instructions }}</p>
+                <p v-for="instruction in instructions">{{ instruction }}</p>
             </div>
         </div>
     </div>
@@ -27,6 +28,14 @@ useAuthRecipeUser()
 const recipeStore = useRecipeStore()
 const route = useRoute()
 const recipe = ref(null)
+
+const instructions = computed(() => {
+    return recipe.value.instructions.split("\n")
+})
+
+const type = computed(() => {
+    return recipe.value.type.charAt(0).toUpperCase() + recipe.value.type.slice(1).toLowerCase()
+})
 
 onMounted(async () => {
     if (recipeStore.recipes.length == 0) {
