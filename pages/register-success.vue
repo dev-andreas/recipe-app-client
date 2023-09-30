60f45bcabf8674184614837b2a5806bd81bd5f0b
Andreas Gerasimow <andreas.gerasimow12@gmail.com>
2023-09-30 20:20:29 +0200

    Register success page

--- pages/register-success.vue
+++ pages/register-success.vue
@@ -1,5 +1,8 @@
 <template>
-    <h1>Success</h1>
+    <div class="flex flex-col items-center gap-10">
+        <h1 class="text-5xl font-bold">Welcome! 🎉</h1>
+        <NuxtLink to="/login" class="btn btn-primary">Sign in</NuxtLink>
+    </div>
 </template>
 
 <script setup>
